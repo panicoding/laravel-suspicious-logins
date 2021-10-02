383404b59f59b0563f2023230bf3c1aa01e27b27
Dave Blakey <dave@blakey.co>
2021-10-02 14:50:04 +0200

    Update suspicious-logins.php

--- config/suspicious-logins.php
+++ config/suspicious-logins.php
@@ -1,13 +1,38 @@
 <?php
 
 return [
+    /**
+     * Save failed logins to the database
+     */
     'record_failed_login' => true,
+
+    /**
+     * Save successful logins to the database
+     * Required to detect suspicious logins
+     */
     'record_successful_login' => true,
 
+    /**
+     * Array of email addresses for an "admin" list of users
+     * who are always notified of a suspicious login
+     */
     'notify_admins' => [],
+
+    /**
+     * Should the user who's account was logged into
+     * be emailed to notify them
+     */
     'notify_users' => true,
+
+    /**
+     * What database column is the users email stored in
+     */
     'email_column' => 'email',
 
+    /**
+     * Should we consider logins in the same city [default true] safe
+     * automatically, and the same country [default false]
+     */
     'safe' => [
         'city' => true,
         'country' => false,
